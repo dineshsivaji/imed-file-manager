14ae950bd6bb01a4d6199cd9dbf7db984279708f
dineshsivaji <007007dinesh@gmail.com>
2025-11-02 14:40:21 +0530

    Added support to open file using monaco-editor

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,11 +4,86 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+// Use the std::fs module for file system operations.
+use std::fs;
+
+// The #[tauri::command] macro exposes this function to the frontend.
+#[tauri::command]
+fn read_file_content(path: String) -> Result<String, String> {
+    // The path argument is passed from the frontend.
+    match fs::read_to_string(path) {
+        // On success, return the file content as a String.
+        Ok(content) => {
+            print!("{}", content);
+            Ok(content)
+        }
+        // On error (e.g., file not found, permission issue), convert the error to a String.
+        Err(e) => Err(format!("Failed to read file: {}", e)),
+    }
+}
+
+// #[tauri::command]
+// async fn pick_file(app_handle: tauri::AppHandle) -> Option<String> {
+//     use tauri_plugin_dialog::DialogExt;
+//     app_handle
+//         .dialog()
+//         .file()
+//         .pick_file()
+//         .await
+//         .map(|path| path.to_string_lossy().to_string())
+// }
+
+use tauri_plugin_dialog::DialogExt;
+#[tauri::command]
+async fn choose_and_read_file(app: tauri::AppHandle) -> Result<String, String> {
+    // let file_path = app.dialog().file().blocking_pick_file().unwrap();
+    // let file_path = app.dialog().file().pick_file(|file_path| {
+    //     // return a file_path `Option`, or `None` if the user closes the dialog
+    //     return file_path;
+    // });
+    // println!("file_path: {:?}", file_path);
+    // let path_buf = file_path.as_path().unwrap().to_path_buf();
+    // let data = std::fs::read(path_buf).unwrap();
+    let file_path = app.dialog().file().blocking_pick_file();
+    println!("file_path: {:?}", file_path);
+
+    match file_path {
+        Some(tauri_path_buf) => {
+            let std_path_buf: std::path::PathBuf = tauri_path_buf.try_into().unwrap();
+
+            match std::fs::read_to_string(std_path_buf) {
+                Ok(data) => Ok(data),
+                Err(e) => Err(format!("Failed to read file: {}", e)),
+            }
+        }
+        None => Err("No file selected".to_string()),
+    }
+}
+
+#[tauri::command]
+fn list_files(path: String) -> Result<Vec<String>, String> {
+    use std::fs;
+    match fs::read_dir(path) {
+        Ok(entries) => Ok(entries
+            .filter_map(|e| e.ok().map(|e| e.path().display().to_string()))
+            .collect()),
+        Err(e) => Err(format!("Failed to list files: {}", e)),
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            read_file_content,
+            list_files,
+            // pick_file,
+            choose_and_read_file
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,48 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import { useState } from 'react';
+import { invoke } from '@tauri-apps/api/core';
+import './App.css';
+import Editor from '@monaco-editor/react';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [greetMsg, setGreetMsg] = useState('');
+  const [name, setName] = useState('');
+  const [code, setCode] = useState<string | null>('{}'); // editor content state
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('read_file_content', { path: name }));
+  }
+
+  async function openFileDialog() {
+    try {
+      const content = await invoke<string | null>('choose_and_read_file');
+      console.log(content);
+      setCode(content);
+    } catch (error) {
+      console.error('Error reading file:', error);
+    }
   }
 
   return (
     <main className="container">
-      <h1>Welcome to Tauri + React</h1>
-
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
       <form
         className="row"
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          openFileDialog();
         }}
       >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
+        <button type="submit">Open</button>
       </form>
       <p>{greetMsg}</p>
+      {/*<FileTree path="/Users/hgd469/Downloads" />*/}
+      <Editor
+        height="90vh"
+        defaultLanguage="json"
+        value={code || ''}
+        onChange={(v) => setCode(v ?? '')}
+        theme="light"
+      />
     </main>
   );
 }
